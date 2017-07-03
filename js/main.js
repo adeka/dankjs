a0ef7b01a11298cb2199e8024abdd43d15281c78
Kyle Montag <thekylemontag@gmail.com>
2017-07-03 15:47:13 -0400

    decouple movement from collision
    :oyes:
    prettier-js on the files touched

--- js/main.js
+++ js/main.js
@@ -1,142 +1,136 @@
-const PIXI = require('pixi.js');
+const PIXI = require("pixi.js");
+require("ui/scss/main.scss");
 
 // react for ui stuff
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-require('ui/scss/main.scss');
-import HUD from 'ui/components/HUD';
+import HUD from "ui/components/HUD";
 
 //game systems
-import Entity from 'js/Entity';
-import RenderSystem from 'js/systems/RenderSystem';
-import MovementSystem from 'js/systems/MovementSystem';
-import InputSystem from 'js/systems/InputSystem';
-import CollisionSystem from 'js/systems/CollisionSystem';
-import OverlapSystem from 'js/systems/OverlapSystem';
-import BotSystem from 'js/systems/BotSystem';
-import InterfaceSystem from 'js/systems/InterfaceSystem';
+import Entity from "js/Entity";
+import RenderSystem from "js/systems/RenderSystem";
+import MovementSystem from "js/systems/MovementSystem";
+import InputSystem from "js/systems/InputSystem";
+import CollisionSystem from "js/systems/CollisionSystem";
+import OverlapSystem from "js/systems/OverlapSystem";
+import BotSystem from "js/systems/BotSystem";
+import InterfaceSystem from "js/systems/InterfaceSystem";
 
-import GameManager from 'js/GameManager';
+import GameManager from "js/GameManager";
 
 let entities = [];
 const ent = new Entity({
-    Renderer: {
-        sprite: 'sunset.png'
+  Renderer: {
+    sprite: "sunset.png"
+  },
+  Position: {
+    x: 30,
+    y: 30
+  },
+  Inventory: {},
+  Input: {
+    axes: {
+      moveVertical: {
+        min: "w",
+        max: "s"
+      },
+      moveHorizontal: {
+        min: "a",
+        max: "d"
+      }
     },
-    Position: {
-        x: 30,
-        y: 30
-    },
-    Inventory: {
-
-    },
-    Input: {
-        axes: {
-            moveVertical: {
-                min: 'w',
-                max: 's'
-            },
-            moveHorizontal: {
-                min: 'a',
-                max: 'd'
-            }
-        },
-        actions: {
-            use: 'e',
-            attack: 'r t g',
-            inventory: 'i'
-        }
-    },
-    Movement: {
-        speed: 3
-    },
-    Collider: {
-        width: 50,
-        height: 50
-    },
-    Overlap: {
-        width: 60,
-        height: 60
+    actions: {
+      use: "e",
+      attack: "r t g",
+      inventory: "i"
     }
+  },
+  Movement: {
+    speed: 3
+  },
+  Collider: {
+    width: 50,
+    height: 50
+  },
+  Overlap: {
+    width: 60,
+    height: 60
+  }
 });
 
 const ent2 = new Entity({
-    Renderer: {
-        sprite: 'plain.png'
-    },
-    Position: {
-        x: 300,
-        y: 80
-    },
-    Movement: {
-        speed: 1
-    },
-    Collider: {
-        width: 50,
-        height: 50
-    },
-    Bot: {
-
-    }
+  Renderer: {
+    sprite: "plain.png"
+  },
+  Position: {
+    x: 300,
+    y: 80
+  },
+  Movement: {
+    speed: 1
+  },
+  Collider: {
+    width: 50,
+    height: 50
+  },
+  Bot: {}
 });
 
 const ent3 = new Entity({
-    Info: {
-        name: 'Fire Pit'
-    },
-    Renderer: {
-        sprite: 'fire.png'
-    },
-    Position: {
-        x: 200,
-        y: 180
-    },
-    Collider: {
-        width: 100,
-        height: 60
-    },
-    Overlap: {
-        width: 150,
-        height: 110
-    }
+  Info: {
+    name: "Fire Pit"
+  },
+  Renderer: {
+    sprite: "fire.png"
+  },
+  Position: {
+    x: 200,
+    y: 180
+  },
+  Collider: {
+    width: 100,
+    height: 60
+  },
+  Overlap: {
+    width: 150,
+    height: 110
+  }
 });
 
 const item = new Entity({
-    Info: {
-        name: 'Scroll'
-    },
-    Renderer: {
-        sprite: 'scroll.png'
-    },
-    Position: {
-        x: 350,
-        y: 225
-    },
-    Overlap: {
-        width: 50,
-        height: 50,
-    }
+  Info: {
+    name: "Scroll"
+  },
+  Renderer: {
+    sprite: "scroll.png"
+  },
+  Position: {
+    x: 350,
+    y: 225
+  },
+  Overlap: {
+    width: 50,
+    height: 50
+  }
 });
 
 const apple = new Entity({
-    Info: {
-        name: 'Dank Old Coin'
-    },
-    Renderer: {
-        sprite: 'coin.png'
-    },
-    Position: {
-        x: 350,
-        y: 325
-    },
-    Overlap: {
-        width: 50,
-        height: 50,
-    },
-    Pickup: {
-
-    }
+  Info: {
+    name: "Dank Old Coin"
+  },
+  Renderer: {
+    sprite: "coin.png"
+  },
+  Position: {
+    x: 350,
+    y: 325
+  },
+  Overlap: {
+    width: 50,
+    height: 50
+  },
+  Pickup: {}
 });
 GameManager.addEntity(ent);
 GameManager.addEntity(ent2);
@@ -155,24 +149,22 @@ let paused = false;
 
 // MAIN GAME LOOP
 (function update() {
-    try {
-        if(!paused) {
-            requestAnimationFrame(update);
+  try {
+    if (!paused) {
+      requestAnimationFrame(update);
 
-            renderer.update(GameManager.entities);
-            bots.update(GameManager.entities);
-            input.update(GameManager.entities);
-            movement.update(GameManager.entities);
-            collision.update(GameManager.entities);
-            overlap.update(GameManager.entities);
-            ui.update(GameManager.entities);
-        }
-    } catch(e) {
-        paused = true;
-        throw new Error(e);
+      renderer.update(GameManager.entities);
+      bots.update(GameManager.entities);
+      input.update(GameManager.entities);
+      movement.update(GameManager.entities);
+      collision.update(GameManager.entities);
+      overlap.update(GameManager.entities);
+      ui.update(GameManager.entities);
     }
+  } catch (e) {
+    paused = true;
+    throw new Error(e);
+  }
 })();
 
-ReactDOM.render(
-    <HUD />
-, document.getElementsByClassName('ui').item(0));
+ReactDOM.render(<HUD />, document.getElementsByClassName("ui").item(0));
--- js/systems/MovementSystem.js
+++ js/systems/MovementSystem.js
@@ -1,38 +1,42 @@
-const PIXI = require('pixi.js');
-import * as Components from 'js/Components';
-import System from 'js/systems/System';
+const PIXI = require("pixi.js");
+import * as Components from "js/Components";
+import System from "js/systems/System";
 
-export default class MovementSystem extends System{
-    constructor() {
-        super();
-    }
-    update(entities) {
-        this.getEntitiesWithComponents(entities,
-            Components.Movement,
-            Components.Position,
-            Components.Collider)
-        .forEach((entity) => {
-            const movement = entity.getComponent(Components.Movement);
-            const position = entity.getComponent(Components.Position);
-            const collider = entity.getComponent(Components.Collider);
+export default class MovementSystem extends System {
+  constructor() {
+    super();
+  }
+  update(entities) {
+    this.getEntitiesWithComponents(
+      entities,
+      Components.Movement,
+      Components.Position
+    ).forEach(entity => {
+      const movement = entity.getComponent(Components.Movement);
+      const position = entity.getComponent(Components.Position);
+      const collider = entity.getComponent(Components.Collider);
 
-            const collision = collider.collision;
-            const newX = position.x + (movement.velocity.x * movement.speed);
-            const newY = position.y + (movement.velocity.y * movement.speed);
+      const newX = position.x + movement.velocity.x * movement.speed;
+      const newY = position.y + movement.velocity.y * movement.speed;
 
-            if(!collision.right && newX > position.x) {
-                position.x = newX;
-            }
-            if(!collision.left && newX < position.x) {
-                position.x = newX;
-            }
-            if(!collision.top && newY < position.y) {
-                position.y = newY;
-            }
-            if(!collision.bottom && newY > position.y) {
-                position.y = newY;
-            }
-
-        });
-    }
+      if (collider) {
+        const collision = collider.collision;
+        if (!collision.right && newX > position.x) {
+          position.x = newX;
+        }
+        if (!collision.left && newX < position.x) {
+          position.x = newX;
+        }
+        if (!collision.top && newY < position.y) {
+          position.y = newY;
+        }
+        if (!collision.bottom && newY > position.y) {
+          position.y = newY;
+        }
+      } else {
+        position.x = newX;
+        position.y = newY;
+      }
+    });
+  }
 }
